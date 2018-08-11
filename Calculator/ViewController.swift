0b7f4853564061d33f1021d58317a94d28ebde4c
Parvi <parvi4love@gmail.com>
2018-08-10 23:39:57 -0400

    Committing at 1:19:37 of the Lecture 1 from Stanford on 10th Aug

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController {  //swift is single inheritance class viewcontroller from UIView Controller, Instances, variable in Swift are called Properties, Functions are Methods
 
-
+    @IBOutlet weak var display: UILabel?
+    //things with ? are optional and get treated as nil by default in Swift while in the case of any properties they must be initiatilized first init
+    var userIsInTheMiddleOfTyping  = false
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) { //type must not be set to Any as it will designate sender as any type instead we would want that to be value
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        }
+        else{
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        //print("\(digit) was touched")
+  }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol{
+            case "pi":
+                display!.text = String(Double.pi)
+            case "sqrt":
+                let x = Double(display!.text!)!
+                display!.text = String(sqrt(x))
+            default:
+                break
+                
+            }
+        }
+    }
+    
+    
 }
 
